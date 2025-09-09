310e6ea09dbe81bcdd4bd4ea3a225ac94e082591
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-09 16:05:27 +0530

    Add expense route to app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify
+import sqlite3
 app = Flask(__name__)
 @app.route('/')
 def working():
     return "Expense Tracker API is working."
+
+#new route for expenses
+@app.route('/expenses')
+def expenses():
+    conn = sqlite3.connect('expense_tracker.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM expenses")
+    all_expenses = cursor.fetchall()
+    expense_json = []
+    for row in all_expenses:
+        expense_json.append({"id": row[0], "item": row[1], "amount": row[2], "category": row[3], "date": row[4]})
+    conn.close()
+    return jsonify(expense_json)
+
 
 
 
